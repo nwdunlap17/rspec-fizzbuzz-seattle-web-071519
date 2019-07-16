af9698a66a32cfb5758de6ff58e028f426c87289
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-16 17:15:26 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(num)
+  string = ""
+  if num%5 == 0
+    string+="fizz"
+  end
+  if num%3 == 0
+    string+="buzz"
+  end
+  if string == ""
+    return NIL
+  else
+    return string
+  end
+end
+
+p fizzbuzz(20)
